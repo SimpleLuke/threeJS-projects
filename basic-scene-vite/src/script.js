d44bc81194691881e1be2b8d72fd6a85276c74df
Luke Lai <lukelai.dev@gmail.com>
2023-04-19 22:26:29 +0100

    Create a group and add 3 cubes in the group

--- basic-scene-vite/src/script.js
+++ basic-scene-vite/src/script.js
@@ -7,12 +7,34 @@ const scene = new THREE.Scene();
 // Axes Helper
 const axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
+/**
+ * Objects
+ */
+const group = new THREE.Group();
+group.scale.y = 2;
+group.rotation.y = 0.2;
+scene.add(group);
 
-//Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -1.5;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 1.5;
+group.add(cube3);
 
 // Sizes
 const sizes = {
